1422b0774b7c99a84560577368a6c8fa7fb58ba7
LamarqueAlexandre <lamarque.alexandre7@gmail.com>
2018-10-15 07:47:06 -0700

    Add dynamic text

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ $(document).ready(function(){
     $('.nav-item').on('click', function(){
         $('.nav-item').removeClass('active');
         $(this).addClass('active');
-    })
+    });
+   
+    // Effet apparition texte lettre par lettre
+    // Trim enlève les espaces au début et à la fin
+    // de la chaine
+
+    var word = $('#display').text().trim();
+
+    // On vide la div qui contient le mot
+    $('#display').text('');
+
+    // Parcourir les lettres du mot
+    for (let i = 0 ; i < word.length; i++) {
+        setTimeout(function() {
+            $('#display').append(word[i]);
+        }, 300 * i);
+    }
 
 });
